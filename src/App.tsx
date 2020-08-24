e6af0c00fedfaaa303ae5bee6116105bcdf9e792
GJ Wang <itellboy@foxmail.com>
2020-08-24 17:10:16 +0800

    change hash router

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { HashRouter, Route } from 'react-router-dom'
 import IndexPage from './pages/IndexPage'
 import InifinityList from './pages/Infinity-List'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route path="/" exact component={IndexPage} />
       <Route path="/infinity-list" exact component={InifinityList} />
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
