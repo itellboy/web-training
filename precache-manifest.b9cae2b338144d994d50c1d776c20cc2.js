849cf153366dd5ec76f6722ff097cd65593d245f
itellboy <itellboy@users.noreply.github.com>
2020-08-26 09:54:11 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.b9cae2b338144d994d50c1d776c20cc2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c39eb194aa20b0c3f86ca8320093538",
+    "url": "/web-training/index.html"
+  },
+  {
+    "revision": "0a775c282dc23485fc0a",
+    "url": "/web-training/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d5e28503fd132627cc13",
+    "url": "/web-training/static/js/2.81de788f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/web-training/static/js/2.81de788f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a775c282dc23485fc0a",
+    "url": "/web-training/static/js/main.50aba747.chunk.js"
+  },
+  {
+    "revision": "e9397f1c384099ae0f86",
+    "url": "/web-training/static/js/runtime-main.b7cdb60c.js"
+  }
+]);
